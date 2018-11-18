84d9c6b4be3486b072cd38a06e8c51a7f4de7825
Hubert Elias <hubelias@gmail.com>
2018-11-18 17:37:25 +0100

    Bootstrap with spring initializr (Kotlin + Gradle) and add .gitignore

---
+++ src/main/kotlin/com/hubelias/parkingmeter/parkingmeterapp/ParkingMeterAppApplication.kt
@@ -0,0 +1,11 @@
+package com.hubelias.parkingmeter.parkingmeterapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ParkingMeterAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<ParkingMeterAppApplication>(*args)
+}
