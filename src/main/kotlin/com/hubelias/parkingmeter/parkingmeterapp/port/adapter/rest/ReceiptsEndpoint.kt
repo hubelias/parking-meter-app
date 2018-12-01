e600e313dfdea8a136859c3f6cb45ec6fa2f0ad3
Hubert Elias <hubelias@gmail.com>
2018-12-01 15:22:08 +0100

    Security configuration (with hardcoded users)

--- src/main/kotlin/com/hubelias/parkingmeter/parkingmeterapp/port/adapter/rest/ReceiptsEndpoint.kt
+++ src/main/kotlin/com/hubelias/parkingmeter/parkingmeterapp/port/adapter/rest/ReceiptsEndpoint.kt
@@ -2,9 +2,12 @@ package com.hubelias.parkingmeter.parkingmeterapp.port.adapter.rest
 
 import com.hubelias.parkingmeter.parkingmeterapp.application.ParkingMeterFacade
 import com.hubelias.parkingmeter.parkingmeterapp.application.ParkingReceiptDto
+import com.hubelias.parkingmeter.parkingmeterapp.port.adapter.users.UserRole
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.security.Principal
+import javax.annotation.security.RolesAllowed
 
 
 @RestController
@@ -13,8 +16,8 @@ class ReceiptsEndpoint(
         private val parkingMeterFacade: ParkingMeterFacade
 ) {
     @GetMapping
-    fun getReceipts(): List<ParkingReceiptDto> {
-        val driverId = "s.hawking" //TODO - as argument
-        return parkingMeterFacade.findDriverReceipts(driverId)
+    @RolesAllowed(UserRole.DRIVER)
+    fun getReceipts(principal: Principal): List<ParkingReceiptDto> {
+        return parkingMeterFacade.findDriverReceipts(principal.name)
     }
 }
