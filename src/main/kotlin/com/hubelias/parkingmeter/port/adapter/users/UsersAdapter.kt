91ebb71439f3e648c0bfcffe141e42d9e637cd9c
Hubert Elias <hubelias@gmail.com>
2018-12-01 15:50:13 +0100

    Use hardcoded users to create drivers

--- src/main/kotlin/com/hubelias/parkingmeter/port/adapter/users/UsersAdapter.kt
+++ src/main/kotlin/com/hubelias/parkingmeter/port/adapter/users/UsersAdapter.kt
@@ -15,29 +15,41 @@ class UsersAdapter : DriverProvider, UserDetailsService {
     private data class UnsafeUser(
             val username: String,
             val password: String,
-            val roles: Set<String>
-    )
+            val isOwner: Boolean = false,
+            val isOperator: Boolean = false,
+            val driverType: Driver.Type? = null)
 
     private val users = listOf(
-            UnsafeUser("admin", "admin", setOf(UserRole.DRIVER, UserRole.OPERATOR, UserRole.OWNER)),
-            UnsafeUser("s.hawking", "universe", setOf(UserRole.DRIVER)),
-            UnsafeUser("jonbonjovi","itsmylife", setOf(UserRole.DRIVER)),
-            UnsafeUser("m.jackson","thriller", setOf(UserRole.OPERATOR)),
-            UnsafeUser("d.trump", "imawesome", setOf(UserRole.OWNER))
+            UnsafeUser("admin", "admin", true, true, Driver.Type.REGULAR),
+            UnsafeUser("s.hawking", "universe", driverType = Driver.Type.DISABLED),
+            UnsafeUser("jonbonjovi", "itsmylife", driverType = Driver.Type.REGULAR),
+            UnsafeUser("m.jackson", "thriller", isOperator = true),
+            UnsafeUser("d.trump", "imawesome", isOwner = true)
     )
 
     override fun getDriver(userId: UserId): Driver? {
-        return Driver(userId, Driver.Type.REGULAR)
+        return findByUsername(userId.id)?.driverType?.let { driverType ->
+            Driver(userId, driverType)
+        }
     }
+
+    private fun findByUsername(username: String) = users.find { it.username == username }
 
     @Throws(UsernameNotFoundException::class)
     override fun loadUserByUsername(username: String): UserDetails {
-        return users.find {it.username == username }?.let { User
-                .withDefaultPasswordEncoder()
-                .username(it.username)
-                .password(it.password)
-                .roles(*it.roles.toTypedArray())
-                .build()
+        return findByUsername(username)?.let {
+            User
+                    .withDefaultPasswordEncoder()
+                    .username(it.username)
+                    .password(it.password)
+                    .roles(*getRoles(it))
+                    .build()
         } ?: throw UsernameNotFoundException("No such user: $username")
     }
+
+    private fun getRoles(user: UnsafeUser) = listOfNotNull(
+            UserRole.DRIVER.takeIf { user.driverType != null },
+            UserRole.OPERATOR.takeIf { user.isOperator },
+            UserRole.OWNER.takeIf { user.isOwner }
+    ).toTypedArray()
 }
